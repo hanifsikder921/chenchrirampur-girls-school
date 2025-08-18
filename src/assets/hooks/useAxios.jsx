c7bf148a6107a3a825b8f64cbef4b2ac4909e034
Md Hanif Sikder <mdhanifsikderpersonal@gmail.com>
2025-08-19 00:11:10 +0600

    scienced subject missign this subject is added

--- src/assets/hooks/useAxios.jsx
+++ src/assets/hooks/useAxios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:5000`
-  // baseURL: `https://chenchri-server.vercel.app`,
+  // baseURL: `http://localhost:5000`
+  baseURL: `https://chenchri-server.vercel.app`,
 });
 
 const useAxios = () => {
